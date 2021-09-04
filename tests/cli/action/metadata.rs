67fdb74b9a87233b85eb4b43e4abad347f224054
Chip Senkbeil <chip@senkbeil.org>
2021-09-03 23:47:57 -0500

    Update metadata request & response to support resolving the file type of symlinks

--- tests/cli/action/metadata.rs
+++ tests/cli/action/metadata.rs
@@ -70,9 +70,42 @@ fn should_support_including_a_canonicalized_path(mut action_cmd: Command) {
     let file = temp.child("file");
     file.touch().unwrap();
 
+    let link = temp.child("link");
+    link.symlink_to_file(file.path()).unwrap();
+
     // distant action metadata --canonicalize {path}
     action_cmd
-        .args(&["metadata", "--canonicalize", file.to_str().unwrap()])
+        .args(&["metadata", "--canonicalize", link.to_str().unwrap()])
+        .assert()
+        .success()
+        .stdout(regex_pred(&format!(
+            concat!(
+                "Canonicalized Path: {:?}\n",
+                "Type: symlink\n",
+                "Len: .*\n",
+                "Readonly: false\n",
+                "Created: .*\n",
+                "Last Accessed: .*\n",
+                "Last Modified: .*\n",
+            ),
+            file.path().canonicalize().unwrap()
+        )))
+        .stderr("");
+}
+
+#[rstest]
+fn should_support_resolving_file_type_of_symlink(mut action_cmd: Command) {
+    let temp = assert_fs::TempDir::new().unwrap();
+
+    let file = temp.child("file");
+    file.touch().unwrap();
+
+    let link = temp.child("link");
+    link.symlink_to_file(file.path()).unwrap();
+
+    // distant action metadata --canonicalize {path}
+    action_cmd
+        .args(&["metadata", "--resolve-file-type", link.to_str().unwrap()])
         .assert()
         .success()
         .stdout(regex_pred(concat!(
@@ -115,6 +148,7 @@ fn should_support_json_metadata_for_file(mut action_cmd: Command) {
         payload: vec![RequestData::Metadata {
             path: file.to_path_buf(),
             canonicalize: false,
+            resolve_file_type: false,
         }],
     };
 
@@ -156,6 +190,7 @@ fn should_support_json_metadata_for_directory(mut action_cmd: Command) {
         payload: vec![RequestData::Metadata {
             path: dir.to_path_buf(),
             canonicalize: false,
+            resolve_file_type: false,
         }],
     };
 
@@ -191,12 +226,57 @@ fn should_support_json_metadata_for_including_a_canonicalized_path(mut action_cm
     let file = temp.child("file");
     file.touch().unwrap();
 
+    let link = temp.child("link");
+    link.symlink_to_file(file.path()).unwrap();
+
     let req = Request {
         id: rand::random(),
         tenant: random_tenant(),
         payload: vec![RequestData::Metadata {
-            path: file.to_path_buf(),
+            path: link.to_path_buf(),
             canonicalize: true,
+            resolve_file_type: false,
+        }],
+    };
+
+    // distant action --format json --interactive
+    let cmd = action_cmd
+        .args(&["--format", "json"])
+        .arg("--interactive")
+        .write_stdin(format!("{}\n", serde_json::to_string(&req).unwrap()))
+        .assert()
+        .success()
+        .stderr("");
+
+    let res: Response = serde_json::from_slice(&cmd.get_output().stdout).unwrap();
+    match &res.payload[0] {
+        ResponseData::Metadata {
+            canonicalized_path: Some(path),
+            file_type: FileType::Symlink,
+            readonly: false,
+            ..
+        } => assert_eq!(path, &file.path().canonicalize().unwrap()),
+        x => panic!("Unexpected response: {:?}", x),
+    }
+}
+
+#[rstest]
+fn should_support_json_metadata_for_resolving_file_type_of_symlink(mut action_cmd: Command) {
+    let temp = assert_fs::TempDir::new().unwrap();
+
+    let file = temp.child("file");
+    file.touch().unwrap();
+
+    let link = temp.child("link");
+    link.symlink_to_file(file.path()).unwrap();
+
+    let req = Request {
+        id: rand::random(),
+        tenant: random_tenant(),
+        payload: vec![RequestData::Metadata {
+            path: link.to_path_buf(),
+            canonicalize: true,
+            resolve_file_type: true,
         }],
     };
 
@@ -214,9 +294,7 @@ fn should_support_json_metadata_for_including_a_canonicalized_path(mut action_cm
         matches!(
             res.payload[0],
             ResponseData::Metadata {
-                canonicalized_path: Some(_),
                 file_type: FileType::File,
-                readonly: false,
                 ..
             },
         ),
@@ -238,6 +316,7 @@ fn should_support_json_output_for_error(mut action_cmd: Command) {
         payload: vec![RequestData::Metadata {
             path: file.to_path_buf(),
             canonicalize: false,
+            resolve_file_type: false,
         }],
     };
 
