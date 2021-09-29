dd4a919bfa428c47cb8c9b248e11ac6d81ae626a
Chip Senkbeil <chip@senkbeil.org>
2021-09-29 18:55:20 -0500

    Initial commit

---
+++ distant-lua/src/lib.rs
@@ -0,0 +1,29 @@
+use mlua::prelude::*;
+
+mod runtime;
+mod session;
+mod utils;
+
+pub use session::{LaunchOpts, Session};
+
+#[mlua::lua_module]
+fn distant_lua(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+
+    // get_session_by_id(id: usize) -> Session
+    exports.set(
+        "get_session_by_id",
+        lua.create_function(|_, id: usize| Ok(Session::new(id)))?,
+    )?;
+
+    // launch(opts: LaunchOpts) -> Session
+    exports.set(
+        "launch",
+        lua.create_async_function(|lua, value: LuaValue| async move {
+            let opts = LaunchOpts::from_lua(value, lua)?;
+            Session::launch(opts).await
+        })?,
+    )?;
+
+    Ok(exports)
+}
