234184b6bbcd934a5e582c0063d6a7f57bf5914c
Chip Senkbeil <chip@senkbeil.org>
2022-06-20 09:48:02 -0500

    Add IntoSplit, TypedAsyncRead, and TypedAsyncWrite impls for (writer, reader)

--- distant-net/src/transport.rs
+++ distant-net/src/transport.rs
@@ -24,6 +24,15 @@ pub trait IntoSplit {
     fn into_split(self) -> (Self::Write, Self::Read);
 }
 
+impl<W, R> IntoSplit for (W, R) {
+    type Write = W;
+    type Read = R;
+
+    fn into_split(self) -> (Self::Write, Self::Read) {
+        (self.0, self.1)
+    }
+}
+
 /// Interface to read some structured data asynchronously
 #[async_trait]
 pub trait TypedAsyncRead<T> {
@@ -32,10 +41,33 @@ pub trait TypedAsyncRead<T> {
     async fn read(&mut self) -> io::Result<Option<T>>;
 }
 
+#[async_trait]
+impl<W, R, T> TypedAsyncRead<T> for (W, R)
+where
+    W: Send,
+    R: TypedAsyncRead<T> + Send,
+{
+    async fn read(&mut self) -> io::Result<Option<T>> {
+        self.1.read().await
+    }
+}
+
 /// Interface to write some structured data asynchronously
 #[async_trait]
 pub trait TypedAsyncWrite<T> {
     async fn write(&mut self, data: T) -> io::Result<()>;
+}
+
+#[async_trait]
+impl<W, R, T> TypedAsyncWrite<T> for (W, R)
+where
+    W: TypedAsyncWrite<T> + Send,
+    R: Send,
+    T: Send + 'static,
+{
+    async fn write(&mut self, data: T) -> io::Result<()> {
+        self.0.write(data).await
+    }
 }
 
 mod router;
