d0ca49542c8457ff189d8c42de1a66a494e23e38
Chip Senkbeil <chip@senkbeil.org>
2022-06-28 11:30:43 +0100

    Move network config to separate file

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,17 @@
-use clap::Args;
 use merge::Merge;
 use serde::{Deserialize, Serialize};
-use std::{
-    io,
-    path::{Path, PathBuf},
-};
+use std::{io, path::Path};
 
 mod client;
 mod common;
 mod manager;
+mod network;
 mod server;
 
 pub use client::*;
 pub use common::*;
 pub use manager::*;
+pub use network::*;
 pub use server::*;
 
 /// Represents configuration settings for all of distant
@@ -36,37 +34,5 @@ impl Config {
         let text = toml_edit::ser::to_string_pretty(self)
             .map_err(|x| io::Error::new(io::ErrorKind::InvalidData, x))?;
         tokio::fs::write(path, text).await
-    }
-}
-
-/// Represents common networking configuration
-#[derive(Args, Debug, Default, Merge, Serialize, Deserialize)]
-pub struct NetworkConfig {
-    /// Unix socket to listen on
-    #[cfg(unix)]
-    #[clap(long)]
-    pub unix_socket: Option<PathBuf>,
-
-    /// Windows pipe to listen on
-    #[cfg(windows)]
-    #[clap(long)]
-    pub windows_pipe: Option<String>,
-}
-
-impl NetworkConfig {
-    /// Returns the custom unix socket path, or the default path
-    #[cfg(unix)]
-    pub fn unix_socket_path_or_default(&self) -> &Path {
-        self.unix_socket
-            .as_deref()
-            .unwrap_or(crate::constants::UNIX_SOCKET_PATH.as_path())
-    }
-
-    /// Returns the custom windows pipe name, or the default name
-    #[cfg(windows)]
-    pub fn windows_pipe_name_or_default(&self) -> &str {
-        self.windows_pipe
-            .as_deref()
-            .unwrap_or(crate::constants::WINDOWS_PIPE_NAME.as_str())
     }
 }
