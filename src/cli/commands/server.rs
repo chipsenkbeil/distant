d89884065b624e7cd374d9000e0ba1a73c057f88
Chip Senkbeil <chip@senkbeil.org>
2022-07-06 15:20:04 -0500

    Some cleanup and an untested attempt at freeing the console to keep child process around

--- src/cli/commands/server.rs
+++ src/cli/commands/server.rs
@@ -21,6 +21,11 @@ pub enum ServerSubcommand {
         #[clap(long)]
         daemon: bool,
 
+        /// If specified, will detach from the console
+        #[cfg(windows)]
+        #[clap(long)]
+        no_console: bool,
+
         /// If specified, the server will not generate a key but instead listen on stdin for the next
         /// 32 bytes that it will use as the key instead. Receiving less than 32 bytes before stdin
         /// is closed is considered an error and any bytes after the first 32 are not used for the key
@@ -30,19 +35,21 @@ pub enum ServerSubcommand {
 }
 
 impl ServerSubcommand {
-    pub fn run(self, config: ServerConfig) -> CliResult<()> {
+    pub fn run(self, _config: ServerConfig) -> CliResult<()> {
         match &self {
-            Self::Listen { daemon, .. } if *daemon => Self::run_daemon(self, config),
+            Self::Listen { daemon, .. } if *daemon => Self::run_daemon(self),
             _ => {
                 let rt = tokio::runtime::Runtime::new()?;
-                rt.block_on(Self::async_run(self, config, false))
+                rt.block_on(Self::async_run(self, false))
             }
         }
     }
 
     #[cfg(windows)]
-    fn run_daemon(self, _config: ServerConfig) -> CliResult<()> {
+    fn run_daemon(self) -> CliResult<()> {
         use std::{
+            ffi::OsString,
+            iter,
             os::windows::process::CommandExt,
             process::{Command, Stdio},
         };
@@ -51,39 +58,45 @@ impl ServerSubcommand {
             .next()
             .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Fork failed"))?;
 
-        // Remove daemon argument to to ensure runs in foreground, otherwise we would fork bomb ourselves
-        let args = args.filter(|arg| {
-            !arg.to_str()
-                .map(|s| s.trim().eq_ignore_ascii_case("--daemon"))
-                .unwrap_or_default()
-        });
+        // Remove --daemon argument to to ensure runs in foreground,
+        // otherwise we would fork bomb ourselves
+        //
+        // Add in --no-console to ensure that our child process properly disassociates itself
+        // so it won't be killed later when a console is terminated
+        let args = args
+            .filter(|arg| {
+                !arg.to_str()
+                    .map(|s| s.trim().eq_ignore_ascii_case("--daemon"))
+                    .unwrap_or_default()
+            })
+            .chain(iter::once(OsString::from("--no-console")));
 
         const DETACHED_PROCESS: u32 = 0x00000008;
         const CREATE_NEW_PROCESS_GROUP: u32 = 0x00000200;
         const CREATE_NO_WINDOW: u32 = 0x08000000;
-        let flags = DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP | CREATE_NO_WINDOW;
+        // let flags = DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP | CREATE_NO_WINDOW;
+        let flags = CREATE_NEW_PROCESS_GROUP;
 
-        // TODO: Can the detached process still communicate to stdout?
         let child = Command::new(program)
             .creation_flags(flags)
             .args(args)
             .stdin(Stdio::null())
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .spawn()?;
         println!("[distant server detached, pid = {}]", child.id());
         Ok(())
     }
 
     #[cfg(unix)]
-    fn run_daemon(self, config: ServerConfig) -> CliResult<()> {
+    fn run_daemon(self) -> CliResult<()> {
         use fork::{daemon, Fork};
 
         // NOTE: We keep the stdin, stdout, stderr open so we can print out the pid with the parent
         match daemon(false, true) {
             Ok(Fork::Child) => {
                 let rt = tokio::runtime::Runtime::new()?;
-                rt.block_on(async { Self::async_run(self, config, true).await })?;
+                rt.block_on(async { Self::async_run(self, true).await })?;
                 Ok(())
             }
             Ok(Fork::Parent(pid)) => {
@@ -98,7 +111,7 @@ impl ServerSubcommand {
         }
     }
 
-    async fn async_run(self, config: ServerConfig, is_forked: bool) -> CliResult<()> {
+    async fn async_run(self, is_forked: bool) -> CliResult<()> {
         match self {
             Self::Listen {
                 config,
@@ -147,8 +160,10 @@ impl ServerSubcommand {
 
                 // Disassociate our server from stdin/stdout/stderr
                 #[cfg(windows)]
-                unsafe {
-                    windows::Win32::System::Console::FreeConsole();
+                if self.no_console {
+                    unsafe {
+                        windows::Win32::System::Console::FreeConsole();
+                    }
                 }
 
                 // For the child, we want to fully disconnect it from pipes, which we do now
