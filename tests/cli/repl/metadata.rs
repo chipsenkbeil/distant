ec641bb29607d29b7092a8de9d562e1ef40e1f0d
Chip Senkbeil <chip@senkbeil.org>
2022-07-14 12:55:26 -0500

    Rewrite metadata json test

--- tests/cli/repl/metadata.rs
+++ tests/cli/repl/metadata.rs
@@ -1,15 +1,7 @@
-use crate::cli::{
-    fixtures::*,
-    utils::{random_tenant, regex_pred, FAILURE_LINE},
-};
-use assert_cmd::Command;
+use crate::cli::fixtures::*;
 use assert_fs::prelude::*;
-use distant::ExitCode;
-use distant_core::{
-    data::{Error, ErrorKind, FileType, Metadata},
-    Request, RequestData, Response, ResponseData,
-};
 use rstest::*;
+use serde_json::{json, Value};
 
 const FILE_CONTENTS: &str = r#"
 some text
@@ -18,53 +10,64 @@ that is a file's contents
 "#;
 
 #[rstest]
-fn should_output_metadata_for_file(mut json_repl: Repl) {
+#[tokio::test]
+async fn should_support_json_metadata_for_file(mut json_repl: Repl) {
     let temp = assert_fs::TempDir::new().unwrap();
 
     let file = temp.child("file");
     file.write_str(FILE_CONTENTS).unwrap();
 
-    // distant action metadata {path}
-    action_cmd
-        .args(&["metadata", file.to_str().unwrap()])
-        .assert()
-        .success()
-        .stdout(regex_pred(concat!(
-            "Type: file\n",
-            "Len: .*\n",
-            "Readonly: false\n",
-            "Created: .*\n",
-            "Last Accessed: .*\n",
-            "Last Modified: .*\n",
-        )))
-        .stderr("");
+    let id = rand::random::<u64>().to_string();
+    let req = json!({
+        "id": id,
+        "payload": {
+            "type": "metadata",
+            "path": file.to_path_buf(),
+            "canonicalize": false,
+            "resolve_file_type": false,
+        },
+    });
+
+    let res = json_repl.write_and_read_json(req).await.unwrap().unwrap();
+
+    assert_eq!(res["origin_id"], id);
+    assert_eq!(res["payload"]["type"], "metadata");
+    assert_eq!(res["payload"]["canonicalized_path"], Value::Null);
+    assert_eq!(res["payload"]["file_type"], "file");
+    assert_eq!(res["payload"]["readonly"], false);
 }
 
 #[rstest]
-fn should_output_metadata_for_directory(mut json_repl: Repl) {
+#[tokio::test]
+async fn should_support_json_metadata_for_directory(mut json_repl: Repl) {
     let temp = assert_fs::TempDir::new().unwrap();
 
     let dir = temp.child("dir");
     dir.create_dir_all().unwrap();
 
-    // distant action metadata {path}
-    action_cmd
-        .args(&["metadata", dir.to_str().unwrap()])
-        .assert()
-        .success()
-        .stdout(regex_pred(concat!(
-            "Type: dir\n",
-            "Len: .*\n",
-            "Readonly: false\n",
-            "Created: .*\n",
-            "Last Accessed: .*\n",
-            "Last Modified: .*\n",
-        )))
-        .stderr("");
+    let id = rand::random::<u64>().to_string();
+    let req = json!({
+        "id": id,
+        "payload": {
+            "type": "metadata",
+            "path": dir.to_path_buf(),
+            "canonicalize": false,
+            "resolve_file_type": false,
+        },
+    });
+
+    let res = json_repl.write_and_read_json(req).await.unwrap().unwrap();
+
+    assert_eq!(res["origin_id"], id);
+    assert_eq!(res["payload"]["type"], "metadata");
+    assert_eq!(res["payload"]["canonicalized_path"], Value::Null);
+    assert_eq!(res["payload"]["file_type"], "dir");
+    assert_eq!(res["payload"]["readonly"], false);
 }
 
 #[rstest]
-fn should_support_including_a_canonicalized_path(mut json_repl: Repl) {
+#[tokio::test]
+async fn should_support_json_metadata_for_including_a_canonicalized_path(mut json_repl: Repl) {
     let temp = assert_fs::TempDir::new().unwrap();
 
     let file = temp.child("file");
@@ -73,28 +76,32 @@ fn should_support_including_a_canonicalized_path(mut json_repl: Repl) {
     let link = temp.child("link");
     link.symlink_to_file(file.path()).unwrap();
 
-    // distant action metadata --canonicalize {path}
-    action_cmd
-        .args(&["metadata", "--canonicalize", link.to_str().unwrap()])
-        .assert()
-        .success()
-        .stdout(regex_pred(&format!(
-            concat!(
-                "Canonicalized Path: {:?}\n",
-                "Type: symlink\n",
-                "Len: .*\n",
-                "Readonly: false\n",
-                "Created: .*\n",
-                "Last Accessed: .*\n",
-                "Last Modified: .*\n",
-            ),
-            file.path().canonicalize().unwrap()
-        )))
-        .stderr("");
+    let id = rand::random::<u64>().to_string();
+    let req = json!({
+        "id": id,
+        "payload": {
+            "type": "metadata",
+            "path": link.to_path_buf(),
+            "canonicalize": true,
+            "resolve_file_type": false,
+        },
+    });
+
+    let res = json_repl.write_and_read_json(req).await.unwrap().unwrap();
+
+    assert_eq!(res["origin_id"], id);
+    assert_eq!(res["payload"]["type"], "metadata");
+    assert_eq!(
+        res["payload"]["canonicalized_path"],
+        json!(file.path().canonicalize().unwrap())
+    );
+    assert_eq!(res["payload"]["file_type"], "symlink");
+    assert_eq!(res["payload"]["readonly"], false);
 }
 
 #[rstest]
-fn should_support_resolving_file_type_of_symlink(mut json_repl: Repl) {
+#[tokio::test]
+async fn should_support_json_metadata_for_resolving_file_type_of_symlink(mut json_repl: Repl) {
     let temp = assert_fs::TempDir::new().unwrap();
 
     let file = temp.child("file");
@@ -103,242 +110,46 @@ fn should_support_resolving_file_type_of_symlink(mut json_repl: Repl) {
     let link = temp.child("link");
     link.symlink_to_file(file.path()).unwrap();
 
-    // distant action metadata --canonicalize {path}
-    action_cmd
-        .args(&["metadata", "--resolve-file-type", link.to_str().unwrap()])
-        .assert()
-        .success()
-        .stdout(regex_pred(concat!(
-            "Type: file\n",
-            "Len: .*\n",
-            "Readonly: false\n",
-            "Created: .*\n",
-            "Last Accessed: .*\n",
-            "Last Modified: .*\n",
-        )))
-        .stderr("");
+    let id = rand::random::<u64>().to_string();
+    let req = json!({
+        "id": id,
+        "payload": {
+            "type": "metadata",
+            "path": link.to_path_buf(),
+            "canonicalize": true,
+            "resolve_file_type": true,
+        },
+    });
+
+    let res = json_repl.write_and_read_json(req).await.unwrap().unwrap();
+
+    assert_eq!(res["origin_id"], id);
+    assert_eq!(res["payload"]["type"], "metadata");
+    assert_eq!(res["payload"]["file_type"], "file");
 }
 
 #[rstest]
-fn yield_an_error_when_fails(mut json_repl: Repl) {
+#[tokio::test]
+async fn should_support_json_output_for_error(mut json_repl: Repl) {
     let temp = assert_fs::TempDir::new().unwrap();
 
     // Don't create file
     let file = temp.child("file");
 
-    // distant action metadata {path}
-    action_cmd
-        .args(&["metadata", file.to_str().unwrap()])
-        .assert()
-        .code(ExitCode::Software.to_i32())
-        .stdout("")
-        .stderr(FAILURE_LINE.clone());
-}
+    let id = rand::random::<u64>().to_string();
+    let req = json!({
+        "id": id,
+        "payload": {
+            "type": "metadata",
+            "path": file.to_path_buf(),
+            "canonicalize": false,
+            "resolve_file_type": false,
+        },
+    });
 
-#[rstest]
-fn should_support_json_metadata_for_file(mut json_repl: Repl) {
-    let temp = assert_fs::TempDir::new().unwrap();
+    let res = json_repl.write_and_read_json(req).await.unwrap().unwrap();
 
-    let file = temp.child("file");
-    file.write_str(FILE_CONTENTS).unwrap();
-
-    let req = Request {
-        id: rand::random(),
-        tenant: random_tenant(),
-        payload: vec![RequestData::Metadata {
-            path: file.to_path_buf(),
-            canonicalize: false,
-            resolve_file_type: false,
-        }],
-    };
-
-    // distant action --format json --interactive
-    let cmd = action_cmd
-        .args(&["--format", "json"])
-        .arg("--interactive")
-        .write_stdin(format!("{}\n", serde_json::to_string(&req).unwrap()))
-        .assert()
-        .success()
-        .stderr("");
-
-    let res: Response = serde_json::from_slice(&cmd.get_output().stdout).unwrap();
-    assert!(
-        matches!(
-            res.payload[0],
-            ResponseData::Metadata(Metadata {
-                canonicalized_path: None,
-                file_type: FileType::File,
-                readonly: false,
-                ..
-            }),
-        ),
-        "Unexpected response: {:?}",
-        res.payload[0],
-    );
-}
-
-#[rstest]
-fn should_support_json_metadata_for_directory(mut json_repl: Repl) {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    let dir = temp.child("dir");
-    dir.create_dir_all().unwrap();
-
-    let req = Request {
-        id: rand::random(),
-        tenant: random_tenant(),
-        payload: vec![RequestData::Metadata {
-            path: dir.to_path_buf(),
-            canonicalize: false,
-            resolve_file_type: false,
-        }],
-    };
-
-    // distant action --format json --interactive
-    let cmd = action_cmd
-        .args(&["--format", "json"])
-        .arg("--interactive")
-        .write_stdin(format!("{}\n", serde_json::to_string(&req).unwrap()))
-        .assert()
-        .success()
-        .stderr("");
-
-    let res: Response = serde_json::from_slice(&cmd.get_output().stdout).unwrap();
-    assert!(
-        matches!(
-            res.payload[0],
-            ResponseData::Metadata(Metadata {
-                canonicalized_path: None,
-                file_type: FileType::Dir,
-                readonly: false,
-                ..
-            }),
-        ),
-        "Unexpected response: {:?}",
-        res.payload[0],
-    );
-}
-
-#[rstest]
-fn should_support_json_metadata_for_including_a_canonicalized_path(mut json_repl: Repl) {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    let file = temp.child("file");
-    file.touch().unwrap();
-
-    let link = temp.child("link");
-    link.symlink_to_file(file.path()).unwrap();
-
-    let req = Request {
-        id: rand::random(),
-        tenant: random_tenant(),
-        payload: vec![RequestData::Metadata {
-            path: link.to_path_buf(),
-            canonicalize: true,
-            resolve_file_type: false,
-        }],
-    };
-
-    // distant action --format json --interactive
-    let cmd = action_cmd
-        .args(&["--format", "json"])
-        .arg("--interactive")
-        .write_stdin(format!("{}\n", serde_json::to_string(&req).unwrap()))
-        .assert()
-        .success()
-        .stderr("");
-
-    let res: Response = serde_json::from_slice(&cmd.get_output().stdout).unwrap();
-    match &res.payload[0] {
-        ResponseData::Metadata(Metadata {
-            canonicalized_path: Some(path),
-            file_type: FileType::Symlink,
-            readonly: false,
-            ..
-        }) => assert_eq!(path, &file.path().canonicalize().unwrap()),
-        x => panic!("Unexpected response: {:?}", x),
-    }
-}
-
-#[rstest]
-fn should_support_json_metadata_for_resolving_file_type_of_symlink(mut json_repl: Repl) {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    let file = temp.child("file");
-    file.touch().unwrap();
-
-    let link = temp.child("link");
-    link.symlink_to_file(file.path()).unwrap();
-
-    let req = Request {
-        id: rand::random(),
-        tenant: random_tenant(),
-        payload: vec![RequestData::Metadata {
-            path: link.to_path_buf(),
-            canonicalize: true,
-            resolve_file_type: true,
-        }],
-    };
-
-    // distant action --format json --interactive
-    let cmd = action_cmd
-        .args(&["--format", "json"])
-        .arg("--interactive")
-        .write_stdin(format!("{}\n", serde_json::to_string(&req).unwrap()))
-        .assert()
-        .success()
-        .stderr("");
-
-    let res: Response = serde_json::from_slice(&cmd.get_output().stdout).unwrap();
-    assert!(
-        matches!(
-            res.payload[0],
-            ResponseData::Metadata(Metadata {
-                file_type: FileType::File,
-                ..
-            }),
-        ),
-        "Unexpected response: {:?}",
-        res.payload[0],
-    );
-}
-
-#[rstest]
-fn should_support_json_output_for_error(mut json_repl: Repl) {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    // Don't create file
-    let file = temp.child("file");
-
-    let req = Request {
-        id: rand::random(),
-        tenant: random_tenant(),
-        payload: vec![RequestData::Metadata {
-            path: file.to_path_buf(),
-            canonicalize: false,
-            resolve_file_type: false,
-        }],
-    };
-
-    // distant action --format json --interactive
-    let cmd = action_cmd
-        .args(&["--format", "json"])
-        .arg("--interactive")
-        .write_stdin(format!("{}\n", serde_json::to_string(&req).unwrap()))
-        .assert()
-        .success()
-        .stderr("");
-
-    let res: Response = serde_json::from_slice(&cmd.get_output().stdout).unwrap();
-    assert!(
-        matches!(
-            res.payload[0],
-            ResponseData::Error(Error {
-                kind: ErrorKind::NotFound,
-                ..
-            })
-        ),
-        "Unexpected response: {:?}",
-        res.payload[0]
-    );
+    assert_eq!(res["origin_id"], id);
+    assert_eq!(res["payload"]["type"], "error");
+    assert_eq!(res["payload"]["kind"], "not_found");
 }
