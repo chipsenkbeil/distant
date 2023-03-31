2b6bf3c0a8a1b51b211dc82f7ff152dfc3e65e0d
Chip Senkbeil <chip@senkbeil.org>
2023-03-31 17:51:43 -0500

    Fix formatting

--- distant-net/src/common/utils.rs
+++ distant-net/src/common/utils.rs
@@ -7,12 +7,8 @@ use std::{fmt, future::Future, io, marker::PhantomData, str::FromStr, time::Dura
 use tokio::{sync::mpsc, task::JoinHandle};
 
 pub fn serialize_to_vec<T: Serialize>(value: &T) -> io::Result<Vec<u8>> {
-    rmp_serde::encode::to_vec_named(value).map_err(|x| {
-        io::Error::new(
-            io::ErrorKind::InvalidData,
-            format!("Serialize failed: {x}"),
-        )
-    })
+    rmp_serde::encode::to_vec_named(value)
+        .map_err(|x| io::Error::new(io::ErrorKind::InvalidData, format!("Serialize failed: {x}")))
 }
 
 pub fn deserialize_from_slice<T: DeserializeOwned>(slice: &[u8]) -> io::Result<T> {
