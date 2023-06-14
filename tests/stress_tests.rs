16eb86ad434a935ed22d349f9797640a99085fe1
Chip Senkbeil <chipsenkbeil@meta.com>
2023-06-14 17:26:45 -0500

    broken stress tests not written

---
+++ tests/stress_tests.rs
@@ -0,0 +1,29 @@
+use assert_fs::prelude::*;
+use predicates::prelude::*;
+use rstest::*;
+
+mod stress;
+
+#[rstest]
+#[test_log::test]
+fn should_handle_large_volume_of_requests() {
+    todo!();
+}
+
+#[rstest]
+#[test_log::test]
+fn should_handle_wide_spread_of_clients() {
+    todo!();
+}
+
+#[rstest]
+#[test_log::test]
+fn should_handle_abrupt_client_disconnects() {
+    todo!();
+}
+
+#[rstest]
+#[test_log::test]
+fn should_handle_badly_killing_client_shell_with_interactive_process() {
+    todo!();
+}
