f896db246cf413e35ed33c9458bbfb256a55fa80
Sam Connelly <myclevorname@gmail.com>
2024-06-26 12:34:25 -0400

    Refactor flake to use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,39 +1,28 @@
 {
   description = "Library and tooling that supports remote filesystem and process operations";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    packages = builtins.listToAttrs (map (systemName: {
-      name = systemName;
-      value = { default =
-        with import nixpkgs { system = systemName; };
-        pkgs.rustPlatform.buildRustPackage rec {
-          name = "distant";
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { system = system; }; {
+        packages.default =
+          pkgs.rustPlatform.buildRustPackage rec {
+            name = "distant";
 
-          # Update this whenever you release a new version. Make sure to omit the 'v'.
-          version = "0.20.0-unstable";
-          src = self;
+            src = self;
 
-          # Update this whenever you update Cargo.lock
-          cargoHash = "sha256-mPcrfBFgvbPi6O7i9FCtN3iaaEOHIcDFHCOpV1NxKMY=";
+            # Update this whenever you update Cargo.lock
+            cargoHash = "sha256-mPcrfBFgvbPi6O7i9FCtN3iaaEOHIcDFHCOpV1NxKMY=";
 
-          # Build time
-          nativeBuildInputs = with pkgs; [ perl ];
+            # Build time
+            nativeBuildInputs = with pkgs; [ perl ];
 
-          meta = {
-            pname = "distant";
-            description = "Library and tooling that supports remote filesystem and process operations.";
-            longDescription = ''
-              Libraries and tooling for working remotely.
-              Deploy distant on your server and connect today to begin remote work!
-            '';
-            homepage = "https://distant.dev";
-            license = with lib.licenses; [ mit asl20 ];
+            meta = {
+              pname = "distant";
+              license = with lib.licenses; [ mit asl20 ];
+            };
           };
-        };
-      };
-    })
-    [ "x86_64-linux" "armv7l-linux"
-      "aarch64-darwin" "x86_64-darwin" ]);
-  };
+      }
+    );
 }
